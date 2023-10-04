c179e01d570a255aef2e2e8b8c8241ac662562c8
duong <daiduong.ducthanh@gmail.com>
2023-10-04 21:01:41 +0700

    reup

--- Controllers/AdminBannerController.cs
+++ Controllers/AdminBannerController.cs
@@ -20,6 +20,17 @@ namespace Hotel.Controllers
         public async Task<IActionResult> Index()
         {
             var data =await _context.Banners.ToListAsync();
+            string messageSuccess = TempData["Success"] as string;
+            if (messageSuccess != null)
+            {
+                ViewData["Success"] = messageSuccess;
+            }
+
+            string messageError = TempData["Error"] as string;
+            if (messageError != null)
+            {
+                ViewData["error"] = messageError;
+            }
 
             return View(data);
         }
@@ -38,11 +49,87 @@ namespace Hotel.Controllers
                 {
                     await _context.AddAsync(new Banner(data, image));
                     await _context.SaveChangesAsync();
+                    TempData["Success"] = "Tạo Banner  thành công";
                     return RedirectToAction("Index");   
                 }
             }
             return View("Create");
             
         }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var Banner = await _context.Banners.FindAsync(id);
+            if (Banner != null)
+            {
+                return View("Update", Banner);
+            }
+            else
+            {
+                ViewData["Fail"] = "Có lỗi xảy ra trong quá trình sử lý . Vui long thử lại";
+                return View("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string content, string title, int id, IFormFile img)
+        {
+            if (img == null)
+            {
+                var banner = await _context.Banners.FindAsync(id);
+                if (banner != null)
+                {
+                    banner.Title = title;
+                    banner.Content = content;
+                    await _context.SaveChangesAsync();
+                    TempData["Success"] = "Banner cập nhập thành công";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Error"] = "Có lỗi xảy ra vui lòng thử lại";
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            else
+            {
+                var NewImage = await CommonMethod.uploadImage(img);
+                if (NewImage != "false")
+                {
+                    var data = await _context.Banners.FindAsync(id);
+                    data.Image = NewImage;
+                    _context.Entry(data).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                    TempData["Success"] = "Banner cập nhập thành công";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Error"] = "Có lỗi xảy ra vui lòng thử lại";
+                    return RedirectToAction("Index");
+                }
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var banner = await _context.Banners.FindAsync(id);
+            if (banner == null)
+            {
+                TempData["Error"] = "Có lỗi xảy ra vui lòng thử lại";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                _context.Entry(banner).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "Banner đã được xáo thành công";
+                return RedirectToAction("Index");
+            }
+
+        }
+
     }
 }
