a35a6d208b767db958985c73e15933cd1b987f54
huyhien <huyhien1989@gmail.com>
2023-10-06 16:21:47 +0900

    Room Detail

--- Controllers/SingleRoomController.cs
+++ Controllers/SingleRoomController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.Data;
+using Hotel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hotel.Controllers
 {
-
+    [Route("SingleRoom/{action}")]
+    [AllowAnonymous]
     public class SingleRoomController : Controller
     {
-        public IActionResult Index()
+        HotelDbContext ctx;
+        public SingleRoomController(HotelDbContext ctx)
         {
-            return View();
+            this.ctx = ctx; 
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            var room = await ctx.Rooms
+                .Include(a => a.Unities)
+                .Include(b => b.roomProperties)
+                .Include(c => c.Images)
+                .Include(d => d.Details)
+                .Include(e => e.RoomType)
+                .SingleOrDefaultAsync(x => x.Id == id);
+            var roomProperties = await ctx.RoomProperties.ToListAsync(); 
+           ViewBag.RoomProperties = new SelectList(roomProperties, "Id", "Name");
+
+            return View(room);
         }
     }
 }
