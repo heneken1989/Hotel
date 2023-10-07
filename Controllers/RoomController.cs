e1f21f8678bbf6b0f47b749a9bb60d42abba9a84
huyhien <huyhien1989@gmail.com>
2023-10-07 16:06:40 +0900

    dddd

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -2,6 +2,7 @@
 using Hotel.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hotel.Controllers
@@ -14,12 +15,28 @@ namespace Hotel.Controllers
         {
             this.ctx = ctx; 
         }
-     
+
         public async Task<IActionResult> Index()
         {
-        
+          
+            var rooms = await ctx.Rooms
 
-            return View();
+                .Include(a => a.RoomType)
+                .Include(a => a.Images!.Take(1))
+            
+                .ToListAsync();
+
+
+            var RoomPro = await ctx.RoomProperties
+
+                 .Include(a => a.Details)
+                 .ToListAsync();
+
+
+            ViewBag.RoomList = new SelectList(rooms, "Id", "Name");
+            ViewBag.RoomProList = new SelectList(RoomPro, "Id", "Name");
+			return View();
+
         }
 
     }
