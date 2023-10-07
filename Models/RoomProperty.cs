dd73842a43015a320b5d4e9ec143b5308b5df595
huyhien <huyhien1989@gmail.com>
2023-10-07 11:20:29 +0900

    Validate

--- Models/RoomProperty.cs
+++ Models/RoomProperty.cs
@@ -1,11 +1,13 @@
-﻿using Hotel.Models.Shared;
+﻿using Hotel.Atribute;
+using Hotel.Models.Shared;
 using System.ComponentModel.DataAnnotations;
 
 namespace Hotel.Models
 {
     public class RoomProperty : BaseEntity
     {
-        [Required]
+        [Required(ErrorMessage = "Cần Nhập Tên Property")]
+        [UniqueName(ErrorMessage = "Tên Property đã tồn tại.")]
         public string? Name { get; set; }
 
         public ICollection<RoomPropertyDetail>? Details { get; set; }
