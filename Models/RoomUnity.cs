bf04a9c4a0eb2c09efd19c10ba52aad44806bfb9
duong <daiduong.ducthanh@gmail.com>
2023-10-07 09:06:43 +0700

    reup again

---
+++ Models/RoomUnity.cs
@@ -0,0 +1,16 @@
+﻿using Hotel.Models.Shared;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hotel.Models
+{
+    public class RoomUnity : BaseEntity
+    {
+        [Required]
+        public string? Name { get; set; }
+        [ForeignKey("Room")]
+        public int? RoomId { get; set; }    
+
+        
+    }
+}
