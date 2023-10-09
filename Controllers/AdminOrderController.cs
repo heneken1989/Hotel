fcaed3e30e77487d2eb66e44c41a27006b648373
huyhien <huyhien1989@gmail.com>
2023-10-09 14:33:15 +0900

    reup

---
+++ Controllers/AdminOrderController.cs
@@ -0,0 +1,48 @@
+﻿using Hotel.Data;
+using Hotel.Dtos;
+using Hotel.Migrations;
+using Hotel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Controllers
+{
+	[Route("admin/order/{action}")]
+	[AllowAnonymous]
+	public class AdminOrderController : Controller
+    {
+        private readonly HotelDbContext _context;
+        public AdminOrderController(HotelDbContext context) { 
+        _context=context;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var data= await  _context.Orders
+                .OrderByDescending(o=>o.CreatedDate)
+                .ToListAsync();
+            return View(data);
+        }
+        [HttpPost]
+        public async Task Update(int id)
+        {
+            var data= await _context.Orders.FindAsync(id);
+            data.IsViewed=true;
+           await _context.SaveChangesAsync();         
+        }
+        public async Task<bool> Add(OrderDto data)
+        {
+            try
+            {
+                await _context.AddAsync(new Order(data));
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+           }
+        }
+    }
+
---
+++ Controllers/SingleRoomController.cs
@@ -0,0 +1,75 @@
+﻿using Hotel.Data;
+using Hotel.Dtos;
+using Hotel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Controllers
+{
+    [Route("SingleRoom/{action}")]
+    [AllowAnonymous]
+    public class SingleRoomController : Controller
+    {
+        HotelDbContext ctx;
+        public SingleRoomController(HotelDbContext ctx)
+        {
+            this.ctx = ctx; 
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            string? message = TempData["success"] as string;
+            if (message != null)
+            {
+                ViewData["success"] = message;
+            }
+
+
+            string? error = TempData["error"] as string;
+            if (error != null)
+            {
+                ViewData["error"] = error;
+            }
+            var room = await ctx.Rooms
+                .Include(a => a.Unities)
+                .Include(b => b.roomProperties)
+                .Include(c => c.Images)
+                .Include(d => d.Details)
+                .Include(e => e.RoomType)
+                .SingleOrDefaultAsync(x => x.Id == id);
+            var roomProperties = await ctx.RoomProperties.ToListAsync(); 
+           ViewBag.RoomProperties = new SelectList(roomProperties, "Id", "Name");
+
+            return View(new RoomDto()
+            {
+                Room=room
+            });
+        }
+
+        public async Task<IActionResult> HandleAddOrder(RoomDto data,int id) 
+        {
+         
+          
+            if (ModelState.IsValid)
+            {
+                AdminOrderController action = new AdminOrderController(ctx);
+                var result =await action.Add(data.Order);
+                if (result == true)
+                {
+                    TempData["success"] = "Thông tin đã được gửi đi thành công. Nhân viên sẽ liên hệ để xác nhận thông tin sớm nhất có thể.Xin cảm ơn !";
+                    return RedirectToAction("Index", new { id = id });
+                }
+                else
+                {
+                    TempData["error"] = "Có lỗi xảy ra vui lòng. nhập lại sau";
+                    return RedirectToAction("Index", new { id=id});
+                }
+
+            }
+            TempData["error"] = "Vui lòng nhập đủ tên và địa chỉ";
+            return RedirectToAction("Index", new { id = id });
+
+        }
+    }
+}
