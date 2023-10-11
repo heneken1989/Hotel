f4f83ce22c0d403b60294a6c5634bc72b4e00acf
huyhien <huyhien1989@gmail.com>
2023-10-11 14:20:14 +0900

    reup again

---
+++ obj/Release/net6.0/PubTmp/Out/wwwroot/ForntEnd/js/modal_delete.js
@@ -0,0 +1,17 @@
+﻿
+
+
+$("#btn-delete-room-type").click(function () {
+    let data_id = $(this).attr("data-id")
+
+    $("#a-link-delete-room-type").attr("href", `/admin/RoomType/delete/?id=${data_id}`)
+});
+
+$(document).ready(() => {
+    $(".paginate_button.next").text("trang tiep theo ")
+    $(".paginate_button.previous").text("trang truoc ")
+    $(".dataTables_info").attr("style","display:none")
+})
+
+
+
