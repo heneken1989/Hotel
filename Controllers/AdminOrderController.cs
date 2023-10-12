3541eb1eabade66fa878e72407bb555e90ce095c
huyhien <huyhien1989@gmail.com>
2023-10-12 12:32:01 +0900

    reup

---
+++ Controllers/AdminOrderController.cs
@@ -0,0 +1,71 @@
+﻿using Hotel.Data;
+using Hotel.Dtos;
+using Hotel.Migrations;
+using Hotel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Controllers
+{
+	[Route("admin/order/{action}")]
+	[AllowAnonymous]
+	public class AdminOrderController : Controller
+    {
+        private readonly HotelDbContext _context;
+        public AdminOrderController(HotelDbContext context) { 
+        _context=context;
+        }
+        public  IActionResult Index()
+        {
+            var data = from o in _context.Orders
+                       join r in _context.Rooms
+                       on o.RoomId equals r.Id into result
+                       from r in result.DefaultIfEmpty()
+                       select new OrderViewDto
+                       {
+                           Name = o.Name,
+                           Phone = o.Phone,
+                           RoomType = r.RoomType!.Type,
+                           Message = o.Message,
+                           Date = o.CreatedDate,
+                           CheckInDate=o.DayCheckin,
+                           OrderId=o.Id,
+                           Isviewed=o.IsViewed
+                       };
+              
+      
+            return View(data.OrderByDescending(o => o.Date));
+            
+        }
+        [HttpPost]
+        public async Task Update(int id)
+        {
+            var data= await _context.Orders.FindAsync(id);
+            data.IsViewed=true;
+           await _context.SaveChangesAsync();         
+        }
+
+        public async Task<bool> Add(OrderDto data)
+        {
+            try
+            {
+                await _context.AddAsync(new Order(data));
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+           }
+
+        public async Task<IActionResult> CountOrder()
+        {
+            var Number =await _context.Orders.CountAsync();
+            return Ok(Number);
+        }
+
+        }
+    }
+
---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Dtos
+{
+    public class LoginDto
+
+    {
+        [Required(ErrorMessage = " User name is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = " Password is required")]
+        public string Password { get; set; }
+    }
+}
