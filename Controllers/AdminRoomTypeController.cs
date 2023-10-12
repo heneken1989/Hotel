6cfd5a4f9d7e6353d18f9cf0f31e932717101848
huyhien <huyhien1989@gmail.com>
2023-10-12 17:44:01 +0900

    reup

---
+++ Controllers/AdminRoomTypeController.cs
@@ -0,0 +1,103 @@
+﻿using Hotel.Data;
+using Hotel.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Controllers
+
+{
+    [Route("admin/RoomType/{action}")]
+    [AllowAnonymous]
+   
+    public class AdminRoomTypeController : Controller
+    {
+        HotelDbContext ctx;
+        public AdminRoomTypeController(HotelDbContext ctx)
+        {
+            this.ctx = ctx; 
+        }
+       
+
+        public async Task<IActionResult> Index()
+        {
+            var types = await ctx.RoomTypes.ToListAsync();
+
+            return View(types);
+        }
+      
+        public  IActionResult Create()
+        {
+
+            return View();  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoomType room)
+        {
+            if(ModelState.IsValid) 
+            {
+                ctx.Entry<RoomType>(room).State = EntityState.Added;
+                await ctx.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Create", room);
+
+            }
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var type = await ctx.RoomTypes.SingleOrDefaultAsync(a => a.Id == id);
+            var room = await ctx.Rooms
+                .Where(r=>r.RoomType.Id == id)
+                .ToListAsync();
+            if(room.Count==0)
+            {
+                ctx.Entry(type!).State = EntityState.Deleted;
+                await ctx.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Không thể xóa vì có tồn tại phòng trong Loại Phòng này!";
+                return RedirectToAction("Index");
+            }
+          
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var type = await ctx.RoomTypes.SingleOrDefaultAsync(a => a.Id == id);
+            return View(type);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoomType Rtype)
+        {
+            if(ModelState.IsValid) 
+            {
+                ctx.Entry(Rtype).State = EntityState.Modified;
+                await ctx.SaveChangesAsync();
+                return Redirect("Index");
+
+            }
+   
+             else
+            {
+                var U = await ctx.RoomTypes.AsNoTracking().SingleOrDefaultAsync(a => a.Id == Rtype.Id);
+                if (Rtype.Type == U.Type)
+                {
+                    return Redirect("Index");
+
+                }
+                return View(Rtype);
+            }
+
+        }
+
+    }
+}
---
+++ Models/RoomType.cs
@@ -0,0 +1,17 @@
+﻿using Hotel.Atribute;
+using Hotel.Models.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Models
+{
+    public class RoomType : BaseEntity
+    {
+
+        [Required(ErrorMessage = "Cần Nhập Type")]
+        [UniqueName(ErrorMessage = "Type đã tồn tại.")]
+        public string? Type { get; set; }
+
+        public ICollection<Room>? Rooms { get;}
+
+    }
+}
